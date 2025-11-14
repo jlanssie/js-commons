65e1bab43b76f65771d6b150f1d386a8ff805962
Jeremy Lanssiers <jlanssie@gmail.com>
2025-11-14 21:47:15 +0100

    update

--- src/url.util.ts
+++ src/url.util.ts
@@ -8,21 +8,21 @@ class UrlUtil {
    * Retrieves the protocol of the current URL, e.g.: 'http:', 'https:'
    */
   static getProtocol(): string {
-    return window.location.protocol;
+    return globalThis.location.protocol;
   }
 
   /**
    * Retrieves the hostname (domain) of the current URL, e.g.: 'www.google.com'
    */
   static getHostName(): string {
-    return window.location.hostname;
+    return globalThis.location.hostname;
   }
 
   /**
    * Retrieves the URL parameters as a URLSearchParams object from the current URL, e.g.: 'https://www.google.com/?q=searchString' => '?q=searchString'
    */
   static getUrlParams(): URLSearchParams {
-    return new URLSearchParams(window.location.search);
+    return new URLSearchParams(globalThis.location.search);
   }
 
   /**
@@ -30,7 +30,7 @@ class UrlUtil {
    * Note: Excludes the '#' character from the result.
    */
   static getAnchor(): string {
-    return window.location.hash.substring(1);
+    return globalThis.location.hash.substring(1);
   }
 }
 
